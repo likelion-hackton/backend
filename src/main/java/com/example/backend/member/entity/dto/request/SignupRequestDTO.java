634a5d541a2dcd998b826c8d151c915d67c4867f
Song Jimin <songwlals050328@gmail.com>
2024-08-30 22:07:49 +0900

    SignupRequestDTO 작성

---
+++ src/main/java/com/example/backend/member/entity/dto/request/SignupRequestDTO.java
@@ -0,0 +1,23 @@
+package com.example.backend.member.entity.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SignupRequestDTO {
+    @NotBlank(message = "이메일이 비어있습니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호가 비어있습니다.")
+    private String password;
+
+    @NotBlank(message = "비밀번호 재입력이 비어있습니다.")
+    private String checkPassword;
+
+    @NotBlank(message = "인증코드가 비어있습니다.")
+    private String verification;
+}
