cc2e12da31b4ddd9e38c5fdf2bc8c3075cb1152a
Song Jimin <songwlals050328@gmail.com>
2024-08-30 21:56:38 +0900

    Member Entity 작성

---
+++ src/main/java/com/example/backend/member/entity/Member.java
@@ -0,0 +1,35 @@
+package com.example.backend.member.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Member")
+@Builder(toBuilder = true)
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String role;
+}
