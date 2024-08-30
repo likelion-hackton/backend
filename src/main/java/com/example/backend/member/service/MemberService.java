6b7fccf2c606c08ceeb53942a3e2a0c303805ae1
Song Jimin <songwlals050328@gmail.com>
2024-08-30 22:16:25 +0900

    Member Service 작성

--- src/main/java/com/example/backend/member/service/MemberService.java
+++ src/main/java/com/example/backend/member/service/MemberService.java
@@ -2,12 +2,17 @@ package com.example.backend.member.service;
 
 import com.example.backend.email.EmailSenderService;
 import com.example.backend.email.EmailVerifyService;
+import com.example.backend.member.entity.Member;
+import com.example.backend.member.entity.dto.MemberDtoConverter;
+import com.example.backend.member.entity.dto.request.SignupRequestDTO;
 import com.example.backend.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -18,4 +23,22 @@ public class MemberService {
     private final EmailSenderService emailSenderService;
     private final EmailVerifyService emailVerifyService;
     private final PasswordEncoder passwordEncoder;
+
+    public void signupMember(SignupRequestDTO req){
+        if(emailVerifyService.verifyCode(req.getEmail(), req.getVerification())){
+            throw new RuntimeException("유효하지 않은 인증코드");
+        }
+        req.setPassword(passwordEncoder.encode(req.getPassword()));
+        memberRepository.save(MemberDtoConverter.signupReqeustConverter(req));
+    }
+
+    public Member getMemberById(Long id){
+        Optional<Member> findMember = memberRepository.findById(id);
+        return findMember.orElse(null);
+    }
+
+    public Member getMemberByEmail(String email){
+        Optional<Member> findMember = memberRepository.findByEmail(email);
+        return findMember.orElse(null);
+    }
 }
