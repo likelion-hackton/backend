21c299d6dfc4e7c289e8fe3b0a66bd9f48390659
Song Jimin <songwlals050328@gmail.com>
2024-08-30 21:58:26 +0900

    Member Repository 작성

---
+++ src/main/java/com/example/backend/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.example.backend.member.repository;
+
+import com.example.backend.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByEmail(String email);
+    Optional<Member> findByEmail(String email);
+}
