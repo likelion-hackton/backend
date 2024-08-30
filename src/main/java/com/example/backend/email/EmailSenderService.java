75bc3d5a5e951c7b6190f7d9ab3adbbd09e3dbc3
Song Jimin <songwlals050328@gmail.com>
2024-08-30 21:42:50 +0900

    인증 이메일 발송 개발

---
+++ src/main/java/com/example/backend/email/EmailSenderService.java
@@ -0,0 +1,42 @@
+package com.example.backend.email;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@Service
+@Transactional
+@Primary
+@RequiredArgsConstructor
+public class EmailSenderService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendVerification(String email, String code){
+        LocalDate nowDate = LocalDate.now();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy. mm. dd");
+
+        LocalTime nowTime = LocalTime.now();
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm:ss");
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("이메일 인증 메일 발송");
+
+        message.setText("""
+                %s
+                
+                %s
+                """
+                .formatted(nowDate.format(dateFormatter) + " " + nowTime.format(timeFormatter),
+                        code));
+        mailSender.send(message);
+    }
+}
