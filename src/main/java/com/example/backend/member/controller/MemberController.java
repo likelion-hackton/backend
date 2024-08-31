e301393ee07d56f67907e9fe8b0977a58c4b2ef0
Song Jimin <songwlals050328@gmail.com>
2024-08-31 23:33:01 +0900

    Member Controller 작성

--- src/main/java/com/example/backend/member/controller/MemberController.java
+++ src/main/java/com/example/backend/member/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.example.backend.member.controller;
 
 import com.example.backend.member.entity.dto.request.EmailVerifyRequestDTO;
+import com.example.backend.member.entity.dto.request.LoginRequestDTO;
+import com.example.backend.member.entity.dto.request.RefreshRequestDTO;
 import com.example.backend.member.entity.dto.request.SignupRequestDTO;
 import com.example.backend.member.entity.dto.response.EmailVerifyResponseDTO;
+import com.example.backend.member.entity.dto.response.JwtTokenResponseDTO;
 import com.example.backend.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,5 +30,17 @@ public class MemberController {
     public ResponseEntity<String> signup(@Valid @RequestBody SignupRequestDTO req){
         memberService.signupMember(req);
         return ResponseEntity.ok("회원가입 성공");
+    }
+
+    @PostMapping("/login")
+    @ResponseBody
+    public ResponseEntity<JwtTokenResponseDTO> login(@Valid @RequestBody LoginRequestDTO req){
+        return ResponseEntity.ok(memberService.login(req));
+    }
+
+    @PostMapping("/refresh")
+    @ResponseBody
+    public ResponseEntity<JwtTokenResponseDTO> refresh(@Valid @RequestBody RefreshRequestDTO req){
+        return ResponseEntity.ok(memberService.verifyRefreshToken(req));
     }
 }
