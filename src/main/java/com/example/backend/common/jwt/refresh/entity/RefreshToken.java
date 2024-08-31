42dbc65e9c6fc3bd9351ed4d3253f96ba335f742
Song Jimin <songwlals050328@gmail.com>
2024-08-31 20:28:37 +0900

    refreshToken entity 작성

---
+++ src/main/java/com/example/backend/common/jwt/refresh/entity/RefreshToken.java
@@ -0,0 +1,33 @@
+package com.example.backend.common.jwt.refresh.entity;
+
+import com.example.backend.member.entity.Member;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "RefreshToken")
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false, unique = true)
+    private String refreshToken;
+
+    @NotNull
+    private Instant refreshExpiresTime;
+}
