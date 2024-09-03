900adc753c8cb2166e5efc9391d7501aa0ec7f70
Song Jimin <songwlals050328@gmail.com>
2024-09-03 17:46:30 +0900

    handler 생성

---
+++ src/main/java/com/example/backend/common/handler/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.example.backend.common.handler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
