43cd06d73a8314f9349662d42faf41f8bab80128
Song Jimin <songwlals050328@gmail.com>
2024-09-07 03:53:04 +0900

    [Feature] LectureConverter 작성

--- src/main/java/com/example/backend/lecture/entity/dto/LectureDtoConverter.java
+++ src/main/java/com/example/backend/lecture/entity/dto/LectureDtoConverter.java
@@ -2,6 +2,7 @@ package com.example.backend.lecture.entity.dto;
 
 import com.example.backend.lecture.entity.Lecture;
 import com.example.backend.lecture.entity.dto.request.CreateLectureRequestDTO;
+import com.example.backend.lecture.entity.dto.response.LectureDetailResponseDTO;
 
 public class LectureDtoConverter {
 
@@ -15,5 +16,17 @@ public class LectureDtoConverter {
                 .dateTime(req.getDateTime())
                 .location(req.getLocation())
                 .build();
+    }
+
+    public static LectureDetailResponseDTO lectureDetailConverter(Lecture lecture){
+        LectureDetailResponseDTO dto = new LectureDetailResponseDTO();
+        dto.setName(lecture.getName());
+        dto.setDescription(lecture.getDescription());
+        dto.setPrice(lecture.getPrice());
+        dto.setType(lecture.getType());
+        dto.setMember_limit(lecture.getMember_limit());
+        dto.setDateTime(lecture.getDateTime());
+        dto.setLocation(lecture.getLocation());
+        return dto;
     }
 }
