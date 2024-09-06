556ebbd0b0ba7149056ba32e7acdcf10938b0d5f
Song Jimin <songwlals050328@gmail.com>
2024-09-07 03:54:50 +0900

    [Feature] LectureService에 강의 생성 작성

--- src/main/java/com/example/backend/lecture/service/LectureService.java
+++ src/main/java/com/example/backend/lecture/service/LectureService.java
@@ -1,5 +1,9 @@
 package com.example.backend.lecture.service;
 
+import com.example.backend.lecture.entity.Lecture;
+import com.example.backend.lecture.entity.dto.LectureDtoConverter;
+import com.example.backend.lecture.entity.dto.request.CreateLectureRequestDTO;
+import com.example.backend.lecture.entity.dto.response.LectureDetailResponseDTO;
 import com.example.backend.lecture.repository.LectureRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -16,4 +20,9 @@ public class LectureService {
     private final LectureRepository lectureRepository;
 
     private static final Logger logger = LoggerFactory.getLogger(LectureService.class);
+
+    public LectureDetailResponseDTO createLecture(CreateLectureRequestDTO req){
+        Lecture saveLecture = lectureRepository.save(LectureDtoConverter.createLectureConverter(req));
+        return LectureDtoConverter.lectureDetailConverter(saveLecture);
+    }
 }
