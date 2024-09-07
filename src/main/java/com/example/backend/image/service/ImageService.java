d643e54e447e444f0215b449c1373f4a3946c365
Song Jimin <songwlals050328@gmail.com>
2024-09-07 23:06:33 +0900

    [Feature] ImageService 생성

---
+++ src/main/java/com/example/backend/image/service/ImageService.java
@@ -0,0 +1,13 @@
+package com.example.backend.image.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@Primary
+@RequiredArgsConstructor
+public class ImageService {
+}
