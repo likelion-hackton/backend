afa54147482188afd77b6ae7ad23fbebe2d637bd
sangkim99 <rettsadd990922@kookmin.ac.kr>
2024-09-08 22:26:02 +0900

    [docs] : Review Entity 추가
    - likeCount/dislikeCount : entity 에서 0L 으로 선언 해줘도 converter 에서 builder 를  통해 데이터베이스에 저장함
      0L로 선언 하는 부분은 converter에서 확인 가능

---
+++ src/main/java/com/example/backend/review/entity/Review.java
@@ -0,0 +1,65 @@
+package com.example.backend.review.entity;
+
+import com.example.backend.lecture.entity.Lecture;
+import com.example.backend.member.entity.Member;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Review")
+@Builder(toBuilder = true)
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String comment;
+
+    @NotNull
+    private Long score;
+
+    @NotNull
+    private LocalDateTime created_at;
+
+    private Long likeCount;  // 기본값 0 설정
+
+    private Long dislikeCount;  // 기본값 0 설정
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lecture_id")
+    private Lecture lecture;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<ReviewImage> reviewImages = new ArrayList<>();
+
+    public void addImage(ReviewImage image) {
+        if (reviewImages == null) {
+            reviewImages = new ArrayList<>();
+        }
+        reviewImages.add(image);
+        image.setReview(this);
+    }
+}
