0ffec237e8c657c3f75cb7373d6df676f709bd5e
sangkim99 <rettsadd990922@kookmin.ac.kr>
2024-09-08 22:29:42 +0900

    [feat] : reviewDetail DTO 추가

---
+++ src/main/java/com/example/backend/review/entity/dto/response/ReviewDetailsDTO.java
@@ -0,0 +1,21 @@
+package com.example.backend.review.entity.dto.response;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ReviewDetailsDTO{
+    @NotBlank(message = "멤버 이름이 비어있습니다.")
+    private String memberNickname;
+
+    private String memberImage;
+
+    @NotBlank(message = "리뷰 내용이 비어있습니다.")
+    private String reviewComment;
+
+    //private String[] reviewImage;
+}
