ab8d4d2f59b8303941569344055dbe2aff9776a6
sangkim99 <rettsadd990922@kookmin.ac.kr>
2024-09-08 22:34:29 +0900

    [feat] : DTO <-> entity 간의 변환을 담당하는 func
    - createReviewConverter : 매개변수로 입력받는 변수들을 ReviewWriteRequestDTO로 변환
    - reviewDetailsConverter : 매개변수로 입력받는 변수들을 이용해 ReviewDetailsDTO 로 변환

---
+++ src/main/java/com/example/backend/review/converter/ReviewConverter.java
@@ -0,0 +1,31 @@
+package com.example.backend.review.converter;
+
+import com.example.backend.lecture.entity.Lecture;
+import com.example.backend.member.entity.Member;
+import com.example.backend.review.entity.Review;
+import com.example.backend.review.entity.dto.request.ReviewWriteRequestDTO;
+import com.example.backend.review.entity.dto.response.ReviewDetailsDTO;
+
+import java.time.LocalDateTime;
+
+public class ReviewConverter {
+
+    public static Review createReviewConverter(ReviewWriteRequestDTO request, Member member, Lecture lecture){
+        return Review.builder()
+                .comment(request.getReviewComment())
+                .score(request.getScore())
+                .created_at(LocalDateTime.now())
+                .member(member)
+                .lecture(lecture)
+                .likeCount(0L)
+                .dislikeCount(0L)
+                .build();
+    }
+
+    public static ReviewDetailsDTO reviewDetailsConverter(Review review, String memberNickname){
+        ReviewDetailsDTO dto = new ReviewDetailsDTO();
+        dto.setReviewComment(review.getComment());
+        dto.setMemberNickname(memberNickname);
+        return dto;
+    }
+}
