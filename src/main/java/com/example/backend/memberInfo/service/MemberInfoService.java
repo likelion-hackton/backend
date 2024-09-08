75352b7b3d6d6d700b92c3ab325205fbd2a6d261
Song Jimin <songwlals050328@gmail.com>
2024-09-09 00:49:08 +0900

    [Feature] MemberInfoService에 editMemberInfo 완성

--- src/main/java/com/example/backend/memberInfo/service/MemberInfoService.java
+++ src/main/java/com/example/backend/memberInfo/service/MemberInfoService.java
@@ -3,6 +3,9 @@ package com.example.backend.memberInfo.service;
 import com.example.backend.image.service.ImageService;
 import com.example.backend.member.entity.Member;
 import com.example.backend.member.repository.MemberRepository;
+import com.example.backend.memberInfo.converter.MemberInfoConverter;
+import com.example.backend.memberInfo.entity.MemberInfo;
+import com.example.backend.memberInfo.entity.MemberInfoImage;
 import com.example.backend.memberInfo.entity.dto.request.EditMemberInfoRequestDTO;
 import com.example.backend.memberInfo.entity.dto.response.MemberInfoDetailResponseDTO;
 import com.example.backend.memberInfo.repository.MemberInfoRepository;
@@ -15,6 +18,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @Primary
@@ -34,7 +39,15 @@ public class MemberInfoService {
             logger.warn("사용자 찾을 수 없음");
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자 찾을 수 없음");
         }
-        imageService.procesAndAddImages();
+        MemberInfo memberInfo = MemberInfoConverter.editMemberInfoConverter(req, member);
+        imageService.procesAndAddImages(memberInfo, List.of(image),
+                url -> {
+                    MemberInfoImage memberInfoImage = new MemberInfoImage();
+                    memberInfoImage.setImageUrl(url);
+                    return memberInfoImage;
+                }, MemberInfo::addImage);
+        MemberInfo saveMemberInfo = memberInfoRepository.save(memberInfo);
+        return MemberInfoConverter.memberInfoDetailConverter(saveMemberInfo);
     }
 
 }
