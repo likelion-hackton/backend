0a856540e85eb61d192df0cb2916a32b15885a9b
Song Jimin <songwlals050328@gmail.com>
2024-09-08 20:20:24 +0900

    [Refactor] Image Service Transactional 변경

--- src/main/java/com/example/backend/email/EmailSenderService.java
+++ src/main/java/com/example/backend/email/EmailSenderService.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
