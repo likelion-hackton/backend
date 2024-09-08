6e29229e9bc77ed375536ad28f093dd0be6066ad
Song Jimin <songwlals050328@gmail.com>
2024-09-08 22:48:27 +0900

    [Feature] MemberInfoService에 editMemberInfo 부분 작성

--- src/main/java/com/example/backend/memberInfo/service/MemberInfoService.java
+++ src/main/java/com/example/backend/memberInfo/service/MemberInfoService.java
@@ -1,11 +1,20 @@
 package com.example.backend.memberInfo.service;
 
+import com.example.backend.image.service.ImageService;
+import com.example.backend.member.entity.Member;
+import com.example.backend.member.repository.MemberRepository;
+import com.example.backend.memberInfo.entity.dto.request.EditMemberInfoRequestDTO;
+import com.example.backend.memberInfo.entity.dto.response.MemberInfoDetailResponseDTO;
 import com.example.backend.memberInfo.repository.MemberInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @Primary
@@ -13,7 +22,19 @@ import org.springframework.stereotype.Service;
 public class MemberInfoService {
 
     private final MemberInfoRepository memberInfoRepository;
+    private final MemberRepository memberRepository;
+    private final ImageService imageService;
 
     private static final Logger logger = LoggerFactory.getLogger(MemberInfoService.class);
+
+    @Transactional
+    public MemberInfoDetailResponseDTO editMemberInfo(EditMemberInfoRequestDTO req, String email, MultipartFile image){
+        Member member = memberRepository.findByEmail(email).orElse(null);
+        if(member == null){
+            logger.warn("사용자 찾을 수 없음");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "사용자 찾을 수 없음");
+        }
+        imageService.procesAndAddImages();
+    }
 
 }
