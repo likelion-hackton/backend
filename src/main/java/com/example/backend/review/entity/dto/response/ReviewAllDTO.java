3d2974d08110d18fe0585b39781c4e5126fb5ca1
sangkim99 <rettsadd990922@kookmin.ac.kr>
2024-09-12 12:30:47 +0900

    [feat] : review All DTO 에 notnull 에노테이션 추가

--- src/main/java/com/example/backend/review/entity/dto/response/ReviewAllDTO.java
+++ src/main/java/com/example/backend/review/entity/dto/response/ReviewAllDTO.java
@@ -1,5 +1,7 @@
 package com.example.backend.review.entity.dto.response;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,20 +12,24 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 public class ReviewAllDTO {
-
-
+    @NotBlank(message = "멤버 이름이 비어있습니다.")
     private String memberNickname;
 
     private String memberImage;
 
+    @NotNull(message = "리뷰 내용이 비어있습니다.")
     private String reviewComment;
 
+    @NotNull(message = "리뷰 점수가 비어있습니다.")
     private Long score;
 
+    @NotNull(message = "리뷰 작성 시간이 비어있습니다.")
     private LocalDateTime createdTime;
 
+    @NotNull(message = "리뷰 좋아요 수가 비어있습니다.")
     private Long likeCount;
 
+    @NotNull(message = "리뷰 싫어요 수가 비어있습니다.")
     private Long dislikeCount;
 
 
