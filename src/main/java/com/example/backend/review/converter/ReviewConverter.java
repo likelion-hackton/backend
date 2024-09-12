f059970c8f047485c9efc87275fecb3461506087
sangkim99 <rettsadd990922@kookmin.ac.kr>
2024-09-12 12:28:45 +0900

    [feat] : review -> reviewAllDTO 로 변환해주는 converter 추가

--- src/main/java/com/example/backend/review/converter/ReviewConverter.java
+++ src/main/java/com/example/backend/review/converter/ReviewConverter.java
@@ -4,9 +4,11 @@ import com.example.backend.lecture.entity.Lecture;
 import com.example.backend.member.entity.Member;
 import com.example.backend.review.entity.Review;
 import com.example.backend.review.entity.dto.request.ReviewWriteRequestDTO;
+import com.example.backend.review.entity.dto.response.ReviewAllDTO;
 import com.example.backend.review.entity.dto.response.ReviewDetailsDTO;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ReviewConverter {
 
@@ -17,8 +19,6 @@ public class ReviewConverter {
                 .created_at(LocalDateTime.now())
                 .member(member)
                 .lecture(lecture)
-                .likeCount(0L)
-                .dislikeCount(0L)
                 .build();
     }
 
@@ -27,5 +27,16 @@ public class ReviewConverter {
         dto.setReviewComment(review.getComment());
         dto.setMemberNickname(memberNickname);
         return dto;
+    }
+
+    public static List<ReviewAllDTO> reviewAllConverter(List<Review> reviews){
+        return reviews.stream().map(review -> {
+            ReviewAllDTO dto = new ReviewAllDTO();
+            dto.setMemberNickname(review.getMember().getEmail());
+            dto.setReviewComment(review.getComment());
+            dto.setScore(review.getScore());
+            dto.setCreatedTime(review.getCreated_at());
+            return dto;
+        }).toList();
     }
 }
