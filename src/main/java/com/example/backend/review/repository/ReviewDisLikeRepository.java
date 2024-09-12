e82bff8c1cc30aa9cfe525bd268c7e52c404b4ec
sangkim99 <rettsadd990922@kookmin.ac.kr>
2024-09-12 22:55:31 +0900

    [feat] : like/dislike repo 에 lecture -> review 로 수정
    - lecture 로 잘못 검색하던 부분들 수정

--- src/main/java/com/example/backend/review/repository/ReviewDisLikeRepository.java
+++ src/main/java/com/example/backend/review/repository/ReviewDisLikeRepository.java
@@ -1,11 +1,12 @@
 package com.example.backend.review.repository;
 
 import com.example.backend.lecture.entity.Lecture;
+import com.example.backend.review.entity.Review;
 import com.example.backend.review.entity.ReviewDisLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewDisLikeRepository extends JpaRepository<ReviewDisLike, Long> {
-    Long findByLectureAndMemberId(Lecture lecture, Long memberId);
+    ReviewDisLike findByReviewAndMemberId(Review review, Long memberId);
 
-    Long countByLecture(Lecture lecture);
+    Long countByReview(Review review);
 }
