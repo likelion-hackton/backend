6bdfa6bf8f40bf8e77f5af606cca891f36ea777e
sangkim99 <rettsadd990922@kookmin.ac.kr>
2024-09-12 22:58:36 +0900

    [feat] : findAllByMember 추가

--- src/main/java/com/example/backend/memberInfo/repository/MemberInfoRepository.java
+++ src/main/java/com/example/backend/memberInfo/repository/MemberInfoRepository.java
@@ -4,9 +4,11 @@ import com.example.backend.member.entity.Member;
 import com.example.backend.memberInfo.entity.MemberInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberInfoRepository extends JpaRepository<MemberInfo, Long> {
     boolean existsByNicknameAndTag(String nickname, String tag);
     Optional<MemberInfo> findByMember(Member member);
+    List<MemberInfo> findAllByMember(Member member);
 }
