554e9628f78e50036fe60abc00b81c44005c75d7
Song Jimin <songwlals050328@gmail.com>
2024-09-14 22:16:36 +0900

    [Feature] CreateOneDayLectureRequestDTO 수정

---
+++ src/main/java/com/example/backend/lecture/entity/dto/request/CreateOneDayLectureRequestDTO.java
@@ -0,0 +1,16 @@
+package com.example.backend.lecture.entity.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateOneDayLectureRequestDTO extends CreateLectureRequestDTO {
+    @NotNull(message = "강의 날짜가 비어있습니다.")
+    private Instant dateTime;
+}
