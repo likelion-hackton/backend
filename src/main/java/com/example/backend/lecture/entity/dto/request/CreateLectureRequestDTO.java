77784c9b65a35baf0f20a3eb34fcb2d5eea94f60
Song Jimin <songwlals050328@gmail.com>
2024-09-14 22:15:51 +0900

    [Feature] CreateLectureRequestDTO 수정

--- src/main/java/com/example/backend/lecture/entity/dto/request/CreateLectureRequestDTO.java
+++ src/main/java/com/example/backend/lecture/entity/dto/request/CreateLectureRequestDTO.java
@@ -17,14 +17,8 @@ public class CreateLectureRequestDTO {
     @NotNull(message = "가격이 비어있습니다.")
     private Long price;
 
-    @NotBlank(message = "강의 유형이 비어있습니다.")
-    private String type;
-
     @NotNull(message = "최대인원이 비어있습니다.")
     private int member_limit;
-
-    @NotNull(message = "강의 날짜가 비어있습니다.")
-    private Instant dateTime;
 
     @NotBlank(message = "강의 위치가 비어있습니다.")
     private String location;
