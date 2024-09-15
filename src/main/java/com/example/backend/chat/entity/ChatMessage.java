2731e3e1207f959c5aa478e1ae3c793ed9f9f5a0
sangkim99 <rettsadd990922@kookmin.ac.kr>
2024-09-15 21:12:09 +0900

    [feat] : 채팅 메세지 entity 추가
    - 채팅방 참여자와 단방향 매핑
      : 채팅방 참여자가 보내는 메세지

---
+++ src/main/java/com/example/backend/chat/entity/ChatMessage.java
@@ -0,0 +1,41 @@
+package com.example.backend.chat.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ChatMessage")
+@Builder(toBuilder = true)
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String message;
+
+    @NotNull
+    private LocalDateTime created_at;
+
+    @NotNull
+    private Boolean isRead;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_participant_id")
+    private ChatParticipant chatParticipant;
+}
