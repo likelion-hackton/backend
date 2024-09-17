e559913bf9fd06047c5d64e4908b25b7a7080159
sangkim99 <rettsadd990922@kookmin.ac.kr>
2024-09-17 21:58:46 +0900

    [feat] : CHAT 메세지 보내기 API 추가
    - /api/chat/send : 메세지 보내기 기능
    - Post, request body 로 데이터 받음

--- src/main/java/com/example/backend/chat/controller/ChatController.java
+++ src/main/java/com/example/backend/chat/controller/ChatController.java
@@ -1,12 +1,15 @@
 package com.example.backend.chat.controller;
 
+import com.example.backend.chat.entity.dto.MessageInfoDTO;
 import com.example.backend.chat.entity.dto.request.CreateChatRoomRequestDTO;
+import com.example.backend.chat.entity.dto.request.SendChatMessageRequest;
 import com.example.backend.chat.entity.dto.response.ChatRoomInfoResponseDTO;
 import com.example.backend.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +22,14 @@ public class ChatController {
 
     // 채팅방 생성
     @PostMapping("/create/chatRoom")
-    public ResponseEntity<ChatRoomInfoResponseDTO> createChatRoom(CreateChatRoomRequestDTO request, Authentication auth){
-        return ResponseEntity.ok(chatService.createChatRoom(request.getLectureId(), auth.getName()));
+    public ResponseEntity<ChatRoomInfoResponseDTO> createChatRoom(@RequestBody CreateChatRoomRequestDTO request, Authentication auth){
+        return ResponseEntity.ok(chatService.createChatRoom(request, auth.getName()));
+    }
+
+    // 메세지 보내기
+    @PostMapping("/send")
+    public ResponseEntity<MessageInfoDTO> sendChatMessage(@RequestBody SendChatMessageRequest request, Authentication auth){
+        return ResponseEntity.ok(chatService.sendChatMessage(request, auth.getName()));
     }
 
 }
